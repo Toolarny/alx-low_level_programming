85db827cbe7bbbff1c0d40aa811de9d880594f22
Toolarny <toolarny@live.com>
2023-07-13 20:10:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <instd.h>
+#include <stdlib.h>
 /**
-* main - A C program that prints a line to the standard error
+* main - A program that prints a line to the std error
 * Return: 1 (Success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
