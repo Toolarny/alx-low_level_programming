d88b5ce3cc0e6f7c2f7216ac986916111ed6c5af
Toolarny <toolarny@live.com>
2023-07-14 09:13:22 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
 * main - A program that prints a line to the std error
 * Return: 1 (Success)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
 return (1);
 }
